1ac5a76897cae43f68942d577aff20b60f78c5a5
LucasRohr <lucasrc17@live.com>
2024-12-20 18:25:03 -0300

    refactor: :recycle: update folder names to reflect SOLID principles

--- solid/principle-3-lsp/dist/main.js
+++ solid/principle-3-lsp/dist/main.js
@@ -4,14 +4,14 @@ var full_time_employee_model_1 = require("./models/full-time-employee-model");
 var researcher_model_1 = require("./models/researcher-model");
 var fullTimeEmployee = new full_time_employee_model_1.FullTimeEmployeeModel('João', 40, 2400);
 var researcher = new researcher_model_1.ResearcherModel('Enzo', 20, fullTimeEmployee);
-// Full Time
+// Full Time Employee
 console.log('name:', fullTimeEmployee.name);
 console.log('gross salary:', fullTimeEmployee.salary);
 console.log('net salary:', fullTimeEmployee.calculateSalary());
 console.log('PS salary:', fullTimeEmployee.calculateProfitSharing(2.5), '\n');
-// Student
+// Researcher
 console.log('name:', researcher.name);
 console.log('workload:', researcher.workload);
 console.log('advisor:', researcher.advisor.name);
-researcher.writeReport();
 researcher.research();
+researcher.writeReport();
